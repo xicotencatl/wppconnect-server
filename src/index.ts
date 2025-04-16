c38ad96ca742e00b148a3d6546bcf96c48aa7867
xicotencatl <oscar.moreno.alcantara@gmail.com>
2025-04-15 18:45:55 -0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -69,8 +69,10 @@ app.use((req, res, next) => {
   const token = authHeader.replace(/^Bearer\s+/i, '').trim(); // Asegura limpieza y sin espacios
   const expectedToken = process.env.TOKEN;
 
-  console.log("TOKEN HEADER =>", token);
-  console.log("TOKEN ESPERADO =>", expectedToken);
+  console.log("TOKEN HEADER crudo =>", authHeader);
+console.log("TOKEN HEADER limpio =>", token);
+console.log("TOKEN ESPERADO =>", expectedToken);
+
 
   if (!token || token !== expectedToken) {
     return res.status(401).json({ message: 'Unauthorized. Token missing or invalid xx' });
