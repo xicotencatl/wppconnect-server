b20caea31373489b9b3c529ca10264519c6ab104
xicotencatl <oscar.moreno.alcantara@gmail.com>
2025-04-16 20:34:38 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import dotenv from 'dotenv';
+dotenv.config();
+
+
 import config from './config';
 import { initServer } from './index';
 
